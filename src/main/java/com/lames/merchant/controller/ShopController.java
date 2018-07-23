66a786afaf3146edf5de086bd1acaca7d1ea0230
fufengchang <3022931383@qq.com>
2018-07-23 08:57:37 +0800

    complete recipe and shop CRUD

--- src/main/java/com/lames/merchant/controller/ShopController.java
+++ src/main/java/com/lames/merchant/controller/ShopController.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpSession;
 
 import com.jake.webmvc.annotation.Controller;
 import com.jake.webmvc.annotation.Mapping;
+import com.lames.merchant.model.JsonResult;
 import com.lames.merchant.po.Shop;
 import com.lames.merchant.service.newVersion.IShopService;
 import com.lames.merchant.service.newVersion.impl.ShopServiceImpl;
+import com.lames.merchant.util.JsonUtil;
 
 @Controller
 @Mapping("/shop")
@@ -23,25 +25,47 @@ public class ShopController {
 		System.out.println(shop);
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		PrintWriter writer = response.getWriter();
-		service.modifyShop(shop);
-		writer.write("{\"status\":\"success\"}");
+		JsonResult result = new JsonResult();
+		if(service.modifyShop(shop) != null) {
+			result.setStatus(true);
+			result.setMessage("修改成功!");
+		}else {
+			result.setStatus(false);
+			result.setMessage("修改失败!");
+		}
+		writer.write(JsonUtil.objectToJson(result));
 	}
 	
 	@Mapping("/list")
 	public void listShop(Shop shop, HttpSession session, HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		PrintWriter writer = response.getWriter();
-		service.findByShopId(shop);
+		JsonResult result = new JsonResult();
+		Shop shop1 = service.findByShopId(shop);
+		if(shop1 != null) {
+			result.setStatus(true);
+			result.setMessage("修改成功!");
+			result.setData("shop", shop1);
+		}else {
+			result.setStatus(false);
+			result.setMessage("修改失败!");
+		}
+		writer.write(JsonUtil.objectToJson(result));
 	}	
 	
 	@Mapping("/save")
 	public void saveShop(Shop shop, HttpSession session, HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		PrintWriter writer = response.getWriter();
-		if(service.saveShop(shop)!=null) {
-			writer.write("{\"status\":\"success\"}");
+		JsonResult result = new JsonResult();
+		if(service.saveShop(shop) != null) {
+			result.setStatus(true);
+			result.setMessage("添加成功!");
 		}else {
-			writer.write("{\"status\":\"fail\"}");
+			result.setStatus(false);
+			result.setMessage("添加失败!");
 		}
-	}	
+		writer.write(JsonUtil.objectToJson(result));
+	}		
+
 }
--- src/main/java/com/lames/merchant/validator/IValidator.java
+++
@@ -1,7 +0,0 @@
-package com.lames.merchant.validator;
-
-import java.util.Map;
-
-public interface IValidator<T> {
-	public Map<String, String> validate (String fieldName, T fieldValue);
-}
